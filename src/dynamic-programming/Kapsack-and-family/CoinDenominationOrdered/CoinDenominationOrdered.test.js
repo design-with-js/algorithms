f6b5eaaae57636c9dc5a5eee52e848c43e16b33a
shubham <shubhammshr621@gmail.com>
2019-04-27 20:35:36 +0530

    tracking untracked files

--- src/dynamic-programming/Kapsack-and-family/CoinDenominationOrdered/CoinDenominationOrdered.test.js
+++
@@ -1,7 +0,0 @@
-import { orderedWays } from "./CoinDenominationOrdered";
-
-test(`Testing Ordered CoinDenomination on 
-  input: [1,2,3], 4, 
-  it should output: 7`, () => {
-  expect(orderedWays([1, 2, 3], 4)).toBe(7);
-});
--- src/dynamic-programming/Kapsack-and-family/CoinDenominationProblem/CoinDenomination.test.js
+++
@@ -1,7 +0,0 @@
-import { ways } from "./CoinDenomination";
-
-test(`Testing CoinDenomination on 
-  input: [1,2,3], 4, 
-  it should output: 4`, () => {
-  expect(ways([1, 2, 3], 4)).toBe(4);
-});
